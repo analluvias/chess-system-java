28c958b1e912711751c95900edfd2834a405b010
Ana Lívia Meira <analiviameira@gmail.com>
2022-07-19 08:42:55 -0300

    knight

--- src/application/UI.java
+++ src/application/UI.java
@@ -63,7 +63,7 @@ public class UI {
 
         System.out.println("Turn: " + chessMatch.getTurn());
 
-        if (!chessMatch.getCheck()){
+        if (!chessMatch.getCheckMate()){
             System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 
             if(chessMatch.getCheck()){
